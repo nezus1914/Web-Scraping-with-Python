a9ac721846b440634c4921ec8d56c6fca5fdde51
Nguyen Minh Quan <minhquannguyen715@gmail.com>
2021-05-19 10:25:19 +0700

    Add files via upload

---
+++ Scraping.py
@@ -0,0 +1,20 @@
+import bs4 as bs
+import requests
+import pickle
+
+html = requests.get("https://en.wikipedia.org/wiki/List_of_S%26P_500_companies")
+soup = bs.BeautifulSoup(html.text)
+
+tickers = []
+
+table = soup.find('table', {'class': 'wikitable sortable'})
+rows = table.findAll('tr')[1:]
+
+for row in rows:
+    ticker = row.findAll('td')[0].text
+    tickers.append(ticker[:-1])
+    
+with open('spn500.pickle', 'wb') as f:
+    pickle.dump(tickers, f)
+    
+   
